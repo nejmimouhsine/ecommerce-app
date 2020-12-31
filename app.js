0c3bd7484ec10d2308eece24f71448df883e136c
nejmimouhsine <nejmi.mouhcine10@gmail.com>
2020-12-31 14:36:57 +0100

    Initial express

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+require("dotenv").config({
+	path: "./config/index.env",
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.send("Welcome to server");
+});
+
+// Page Not Founded
+app.use((req, res) => {
+	res.status(404).json({
+		msg: "Page not found",
+	});
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
