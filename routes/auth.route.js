b340da080c6334c92953e61d3a3320259b7467e6
nejmimouhsine <nejmi.mouhcine10@gmail.com>
2020-12-31 19:46:53 +0100

    login user and authentication middleware

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,9 +5,24 @@ const bcrypt = require("bcryptjs"); // encrypt password
 // Check validation for requests
 const { check, validationResult } = require("express-validator");
 const gravatar = require("gravatar"); // get user image by email
+const auth = require("../middleware/auth");
 
 // Models
 const User = require("../models/User");
+
+// @route   POST api/user
+// @desc    User Information
+// @access  Private
+router.get("/", auth, async (req, res) => {
+	try {
+		// get user information by id
+		const user = await User.findById(req.user.id).select("password");
+		res.json(user);
+	} catch (error) {
+		console.log(error.message);
+		res.status(500).send("Server error");
+	}
+});
 
 // @route   POST api/user/register
 // @desc    Register user
@@ -99,4 +114,78 @@ router.post(
 	}
 );
 
+// @route   POST api/user/login
+// @desc    Login user
+// @access  Public
+router.post(
+	"/login",
+	[
+		// Validation for email & password
+		check("email", "please include a valid email").isEmail(),
+		check("password", "password is required").exists(),
+	],
+	async (req, res) => {
+		// if error exists
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({
+				errors: errors.array(),
+			});
+		}
+		// if everything is ok
+		const { email, password } = req.body;
+
+		try {
+			// find user
+			let user = await User.findOne({ email });
+
+			// if user not found in database
+			if (!user) {
+				return res.status(400).json({
+					errors: [
+						{
+							msg: "Invalid Credentials",
+						},
+					],
+				});
+			}
+
+			// Know user founded by email let's compare passwords
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			// Passwords don't match
+			if (!isMatch) {
+				return res.status(400).json({
+					errors: [
+						{
+							msg: "Invalid Credentials",
+						},
+					],
+				});
+			}
+
+			// Payload for jwt
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				process.env.JWT_SECRET,
+				{
+					expiresIn: 360000,
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (error) {
+			console.log(error.message);
+			res.status(500).send("Server error");
+		}
+	}
+);
 module.exports = router;
